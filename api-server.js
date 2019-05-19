aef458f617507bdecde8b704a61b6c66d5513b0c
Serik Shaikamalov <serik.shaikamalov@nt.local>
2019-05-20 01:33:46 +0600

    optimize code

--- api-server.js
+++ api-server.js
@@ -2,44 +2,27 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 const constants = require('./config/constants');
 const usersRoutes = require('./routes/user.route');
 const ramazanRoutes = require('./routes/ramazan.route');
-
 const app = express();
 const port = 4000;
 
-
-/**
- * Each request goes
- * 1. Middleware: Check user token
- * 2. Middleware: Check user permission
- * 3. Middleware: Return result
- */
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-/**
- * Middleware: Resource
- */
+// Routes
 app.use('/users', usersRoutes);
 app.use('/ramazan', ramazanRoutes);
 
-/**
- * App: Start
- */
+// App: Start 
 app.listen(port, (error) => {
-
+    console.info(`App start`);
     if (error) console.error(error);
-    const connectionString = constants.DB_CONNECTION;
-
-    mongoose.connect(connectionString, (err) => {
-        if (err) {
-            console.log(err);
-        }
+    
+    mongoose.connect(constants.DB_CONNECTION, (err) => {
+        if (err) console.error(err);        
         console.log(`Example app listening on port ${port}!`);
     });
 });
